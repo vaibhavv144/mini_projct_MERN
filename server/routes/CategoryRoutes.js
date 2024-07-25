7aa07b8df380db51d6b2d474f7a5da531e89a006
Vaibhav <vaibhavsinggh144@gmail.com>
2024-07-26 02:32:35 +0530

    chg

--- server/routes/CategoryRoutes.js
+++ server/routes/CategoryRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from "../controllers/CategoryController.js";
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from "../controllers/categoryController.js";
 
 
 const router = express.Router()
